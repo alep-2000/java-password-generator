323dba6bddb9af9495fe7ec65c2bc25be61b6ad7
alep-2000 <alessiopiras056@gmail.com>
2023-11-09 16:34:36 +0100

    dichiarazione variabili

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -2,7 +2,12 @@ package org.lessons.java.security;
 
 public class PasswordGenerator {
 	public static void main(String[] args) {
-		
+		String nome = "Francesco";
+		String cognome = "Casula";
+		String colore_preferito = "Rosso";
+		byte giorno = 18;
+		byte mese = 11;
+		short anno = 2000;
 		
 	}
 }
